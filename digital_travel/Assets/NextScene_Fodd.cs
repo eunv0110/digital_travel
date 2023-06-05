d0556856b4b477c814fa07be3fc6765ad3ffd856
eunv0110 <134673146+eunv0110@users.noreply.github.com>
2023-06-05 17:02:45 +0900

    공연예매까지
    공연예매까지개발 완료

---
+++ digital_travel/Assets/NextScene_Fodd.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class NextScene_Fodd : MonoBehaviour
+{
+    public void NextPlayScene()
+    {
+        SceneManager.LoadScene("Food_Play");
+    }
+}
